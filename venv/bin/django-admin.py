bc424b435d37db713c6c02126d8df6e3c3665aed
Jackson Marcony <50585473+JacksonMarcony@users.noreply.github.com>
2021-08-14 11:52:31 -0300

    Delete venv directory

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jackson/Área de Trabalho/Estudos/Python/Python_Django/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
